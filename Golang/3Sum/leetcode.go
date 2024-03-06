a2e95e2b594974858f2388e8abf1529d43db7349
Lorenzo Drumond <lorenzo.drumond@neomediaworld.com>
2024-03-06 10:41:03 +0000

    Better solution, beats 62%, not mine

--- Golang/3Sum/leetcode.go
+++ Golang/3Sum/leetcode.go
@@ -4,35 +4,49 @@
 // Notice that the solution set must not contain duplicate triplets.
 package leetcode
 
-import "slices"
+import "sort"
 import "fmt"
 
 func ThreeSum(nums []int) [][]int {
-	result := [][]int{}
-	tracker := make(map[[3]int]bool)
+	var res [][]int
+
+	if len(nums) < 3 {
+		return res
+	}
+
+	sort.Ints(nums)
 
 	for i := 0; i < len(nums)-2; i++ {
-		ledger := make(map[int]int)
-		el := nums[i]
-		for _, num := range nums[i+1:] {
-			val, ok := ledger[num]
-			if ok {
-				finalArray := [3]int{el, val, num}
-				finalSlice := finalArray[:]
-				slices.Sort(finalSlice)
-				_, ok2 := tracker[finalArray]
-				if ok2 {
-					continue
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left, right := i+1, len(nums)-1
+
+		for left < right {
+			target := nums[i] + nums[left] + nums[right]
+
+			if target == 0 {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				left, right = left+1, right-1
+
+				for left < right && nums[left] == nums[left-1] {
+					left++
 				}
-				result = append(result, finalSlice)
-				tracker[finalArray] = true
-				continue
+
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			} else if target > 0 {
+				right--
+			} else {
+				left++
 			}
-			ledger[-el-num] = num
 		}
 	}
-	fmt.Println(result)
-	return result
+
+	fmt.Println(res)
+	return res
 }
 
 func TwoSum(input []int, sum int) [][]int {
