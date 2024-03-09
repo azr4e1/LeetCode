c0fb321d8003edc600c6fff2ca835cc7279afe12
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-03-09 16:51:33 +0000

    Slow solution

--- Golang/LongestPalindromicSubstring/leetcode.go
+++ Golang/LongestPalindromicSubstring/leetcode.go
@@ -1,8 +1,27 @@
 // Given a string s, return the longest palindromic substring in s.
 package leetcode
 
-import ()
-
 func LongestPalindrome(s string) string {
-	return ""
+	if len(s) <= 1 {
+		return s
+	}
+	var substring string
+	var reverseSubstring string
+	var longestPal string
+	for i := 0; i < len(s); i++ {
+		substring = string(s[i])
+		reverseSubstring = substring
+		for j := i + 1; j < len(s); j++ {
+			substring = substring + string(s[j])
+			reverseSubstring = string(s[j]) + reverseSubstring
+			if substring == reverseSubstring && len(substring) > len(longestPal) {
+				longestPal = substring
+			}
+		}
+	}
+
+	if longestPal == "" {
+		longestPal = string(s[0])
+	}
+	return longestPal
 }
