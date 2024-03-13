32150ccb3ba2642dfeef7cf20a8bd706baf1c43a
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-03-13 00:39:10 +0000

    chore: commit save point

--- Golang/RotateArray/leetcode.go
+++ Golang/RotateArray/leetcode.go
@@ -25,10 +25,25 @@ func Rotate3(nums []int, k int) {
 	k %= l
 	var prev int
 	for i := 0; i < k*l; i++ {
-		// prev = nums[(i+1)%l]
 		if i%l == 0 {
 			prev = nums[l-1]
 		}
 		prev, nums[i%l] = nums[i%l], prev
+	}
+}
+
+func Rotate4(nums []int, k int) {
+	n := len(nums)
+	k %= n
+	reverse(nums, 0, n-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, n-1)
+}
+
+func reverse(nums []int, start int, end int) {
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
 	}
 }
