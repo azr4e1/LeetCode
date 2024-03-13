197b1d084b08a458f4604cc863293f200c6acae4
Lorenzo Drumond <lorenzo.drumond@neomediaworld.com>
2024-03-13 15:15:44 +0000

    chore: commit save point

--- Golang/JumpGame/leetcode.go
+++ Golang/JumpGame/leetcode.go
@@ -2,13 +2,7 @@ package leetcode
 
 func CanJump(jumps []int) bool {
 	jump := jumps[0]
-	if len(jumps) == 1 {
-		return true
-	}
-	if jump == 0 {
-		return false
-	}
-	if jump >= len(jumps) {
+	if jump >= len(jumps)-1 {
 		return true
 	}
 	var result bool
