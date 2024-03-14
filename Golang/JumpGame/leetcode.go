d2b8413e9d3621b4686e798b8fae59cb0bd4b565
Lorenzo Drumond <lorenzo.drumond@neomediaworld.com>
2024-03-14 09:56:47 +0000

    Memoized function solved the issue of speed

--- Golang/JumpGame/leetcode.go
+++ Golang/JumpGame/leetcode.go
@@ -1,16 +1,29 @@
 package leetcode
 
-func CanJump(jumps []int) bool {
+func canJumpCache(jumps []int, cache *map[int]bool) bool {
+	if val, ok := (*cache)[len(jumps)]; ok {
+		return val
+	}
 	jump := jumps[0]
 	if jump >= len(jumps)-1 {
+		(*cache)[len(jumps)] = true
 		return true
 	}
 	var result bool
 	for i := min(jump, len(jumps)-1); i >= 1; i-- {
-		result = CanJump(jumps[i:])
+		result = canJumpCache(jumps[i:], cache)
 		if result {
+			(*cache)[len(jumps)] = true
 			return true
 		}
 	}
+	(*cache)[len(jumps)] = false
 	return false
+}
+
+func CanJump(jumps []int) bool {
+	cache := make(map[int]bool)
+	result := canJumpCache(jumps, &cache)
+
+	return result
 }
