54abc80899572651b79e32d09865d98066be37b1
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-03-17 15:43:53 +0000

    chore: commit save point

--- Golang/ContiguousArray/leetcode.go
+++ Golang/ContiguousArray/leetcode.go
@@ -2,5 +2,22 @@
 package leetcode
 
 func FindMaxLength(nums []int) int {
-	return 0
+	maxLength := 0
+	n := len(nums)
+	sums := make([]int, n)
+	sums[0] = nums[0]
+	for i := 1; i < n; i++ {
+		sums[i] = sums[i-1] + nums[i]
+	}
+
+	for i := 0; i < n-1; i++ {
+		initialPos := sums[i]
+		for j := maxLength + i + 1; j < n; j += 2 {
+			length := j - i + 1
+			if (sums[j]-initialPos+nums[i]) == length/2 && length > maxLength {
+				maxLength = length
+			}
+		}
+	}
+	return maxLength
 }
--- Golang/ContiguousArray/leetcode_test.go
+++ Golang/ContiguousArray/leetcode_test.go
@@ -1,7 +1,9 @@
 package leetcode_test
 
 import (
+	"encoding/json"
 	"leetcode"
+	"os"
 	"testing"
 )
 
@@ -12,17 +14,27 @@ func TestFindMaxLength(t *testing.T) {
 		Answer   int
 	}
 
+	bigTestCase := []int{}
+	file, err := os.Open("testdata/bigArray.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = json.NewDecoder(file).Decode(&bigTestCase)
+	if err != nil {
+		t.Fatal(err)
+	}
 	testCases := []TestCase{
 		{Question: []int{0, 1}, Answer: 2},
 		{Question: []int{0, 1, 0}, Answer: 2},
+		{Question: bigTestCase, Answer: 44044},
 	}
 
-	for _, tc := range testCases {
+	for index, tc := range testCases {
 		want := tc.Answer
 		got := leetcode.FindMaxLength(tc.Question)
 
 		if want != got {
-			t.Errorf("want %d, got %d for test case %v", want, got, tc.Question)
+			t.Errorf("want %d, got %d for test case no. %d", want, got, index+1)
 		}
 	}
 }
