fd9fc3a28d13f2ebc1f6a5e90839e8e44c6d8e1a
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-03-17 16:16:28 +0000

    chore: commit save point

--- Golang/ContiguousArray/leetcode.go
+++ Golang/ContiguousArray/leetcode.go
@@ -21,3 +21,26 @@ func FindMaxLength(nums []int) int {
 	}
 	return maxLength
 }
+
+// Better solution
+func findMaxLength(nums []int) int {
+	hashmap := make(map[int]int)
+	zeros, ones, maxLen := 0, 0, 0
+	hashmap[0] = -1
+
+	for i, num := range nums {
+		if num == 0 {
+			zeros++
+		} else {
+			ones++
+		}
+		diff := zeros - ones
+		if val, ok := hashmap[diff]; ok {
+			maxLen = max(maxLen, i-val)
+		} else {
+			hashmap[diff] = i
+		}
+	}
+
+	return maxLen
+}
